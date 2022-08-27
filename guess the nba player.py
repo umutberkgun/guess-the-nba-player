ceef87c620613a1ac5e7cfda2cf83de8c49b1649
umutberkgun <umutberkgunnebioglu@gmail.com>
2022-08-27 15:33:21 +0300

    Create guess the nba player.py

---
+++ guess the nba player.py
@@ -0,0 +1,57 @@
+import random
+
+
+liste = ["Kobe Bryant","Lebron James","Kevin Durant","Michael Jordan","Larry Bird","Stephen Curry",
+"Tim Duncan", "Wilt Chamberlain","Bill Russel","Giannis Antetokounmpo","Kareem Abdul-Jabbar","Magic Johnson","Shaq"]
+
+liste = [i.upper() for i in liste]
+
+
+
+
+
+answer = random.choice(liste)
+
+counter= 5
+
+print("""
+***************************
+GUESS THE TOP 10 NBA PLAYER
+
+Guess one of the top 10 NBA Players of all time. 
+You have 5 guesses!
+
+***************************
+""")
+
+
+
+
+while True:
+    guess = (input("which top 10 nba player is this?"))
+
+    for i in range(len(guess)):
+        guess = guess.upper()
+
+
+    if guess == answer:
+        print("congrats!")
+        break
+    elif guess != answer:
+        counter -= 1
+        print("Wrong! You have {} guesses left" .format(counter))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
